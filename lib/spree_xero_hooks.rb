e77adecd7b864c6f7b5ac2503f3d35787299c6d4
peterfealey <peter.fealey@feeldigital.co.uk>
2015-03-31 14:54:28 +0100

    model decorators to follow spree 2 conventions

--- lib/spree_xero_hooks.rb
+++ lib/spree_xero_hooks.rb
@@ -1,9 +1,8 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
-                       :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+Deface::Override.new(:virtual_path => "spree/admin/general_settings/index",
+                       :insert_after => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
                        :text => "
         <tr>
           <td><%= link_to t('xero_settings'), admin_xero_settings_path %></td>
           <td><%= t(\"xero_settings\") %></td>
         </tr>
-      ",
-                       :disabled => false)
+      ", :disabled => false)
