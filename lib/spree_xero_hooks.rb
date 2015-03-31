00b203782c24e844325451980eaaef27435db0bc
peterfealey <peter.fealey@feeldigital.co.uk>
2015-03-31 15:38:14 +0100

    Allow all prefs

--- lib/spree_xero_hooks.rb
+++ lib/spree_xero_hooks.rb
@@ -1,6 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/general_settings/edit",
-                       :insert_after => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                       :text => "
+Deface::Override.new(:virtual_path => "spree/admin/configurations/index", :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]", :text => "
         <tr>
           <td><%= link_to t('xero_settings'), admin_xero_settings_path %></td>
           <td><%= t(\"xero_settings\") %></td>
